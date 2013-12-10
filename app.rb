b79d28c114d9a2c6e57a8e5142c14a3c7f3e65fa
anisharamnani <anisha.ramnani@gmail.com>
2013-12-10 15:40:35 -0500

    working rps

--- app.rb
+++ app.rb
@@ -1,21 +1,14 @@
+require_relative './lib/game'
 require 'sinatra'
+
 
 # Why is it a good idea to wrap our App class in a module?
 class RPS_App < Sinatra::Application
-
-		get '/throw/' do
-			erb :throw 
-  	end 
   	
   	get '/throw/:type' do
-  	
-  	computer = ["rock", "paper", "scissor"].sample
-  	"You chose #{params[:type]} and computer chooses #{computer}."
-
-  	if computer == "#{params[:type]}"
-  		"Draw"
-  	else 
-  		"You lose"
-  	end 
-  end
+  		@game = Game.new("#{params[:type]}")
+  		@message = @game.message
+  		@score = @game.score
+  		erb :move
+  	end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,23 +1,31 @@
  	class Game 
 
- 		def initialize 
+ 		attr_accessor :player_move 
+ 		attr_reader :computer 
+
+ 		def initialize move 
  			@player_move = move 
- 		end 
-
- 		case player_moves
- 			when player_move = "rock"
- 				return "You win" if computer == "scissors"
- 				return "You lose!" if computer = "paper"
-
- 			when player_move = "scissors"
-				return "You win" if computer == "paper"
- 				return "You lose!" if computer = "rock"
- 			when player_move = "paper"
-				return "You win" if computer == "rock"
- 				return "You lose!" if computer = "scissors"
- 			else 
- 				return "Draw!"
+ 			@computer = ["rock", "paper", "scissors"].sample 
  		end
 
+ 		def message 
+ 			return "Your move is #{player_move}. The computer chose #{computer}."
+ 		end 
+
+ 		def score 
+	 		case player_move
+	 			when computer
+	 				return "Draw"
+	 			when "rock"
+	 				return "You win!" if computer == "scissors"
+	 				return "You lose!" if computer == "paper"
+	 			when "scissors"
+					return "You win!" if computer == "paper"
+	 				return "You lose!" if computer == "rock"
+	 			when "paper"
+					return "You win!" if computer == "rock"
+	 				return "You lose!" if computer == "scissors"
+	 		end
+ 		end 
 
  	end 
